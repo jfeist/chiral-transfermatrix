ed499e8a3987442737b0e06036364615fb0fba9f
Johannes Feist <johannes.feist@gmail.com>
2023-06-06 09:59:01 +0200

    further cleanup

--- tests/test_3.py
+++ tests/test_3.py
@@ -29,10 +29,10 @@ def eps_DL(epsinf, omegap, omega, omega0=0, gamma=0, k0=0):
 # RANGE OF OMEGA AND CREATION OF THE CORRESPONDING ARRAY FOR THE INPUT
 ######################################################################
 omega = np.linspace(1.8, 2.2, 30)
-ngrid = np.ones_like(omega)
 ######################################################################
 
 def chirality_preserving_mirror_scatmat(omegaPR,gammaPR,omega,reversed=False):
+    ngrid = np.ones_like(omega)
     tP =  gammaPR / (1j * (omega - omegaPR) + gammaPR)
     rM = abs(tP)
     phase = tP / rM
@@ -43,44 +43,39 @@ def chirality_preserving_mirror_scatmat(omegaPR,gammaPR,omega,reversed=False):
 
     tPP_r = t * pst
     tMM_r = t * pst
-    if reversed:
-        tMP_r = tPM * phase
-        tPM_r = 0.0j * ngrid
-    else:
-        tMP_r = 0.0j * ngrid
-        tPM_r = tPM * phase
-
     tPP_l = t * pst
     tMM_l = t * pst
     if reversed:
+        tMP_r = tPM * phase
+        tPM_r = 0.0j * ngrid
         tMP_l = 0.0j * ngrid
         tPM_l = tPM * phase
     else:
+        tMP_r = 0.0j * ngrid
+        tPM_r = tPM * phase
         tMP_l = tPM * phase
         tPM_l = 0.0j * ngrid
 
     if reversed:
         rPP_r = 0.0j * ngrid
         rMM_r = tPM * pst**4 / phase**3
-    else:
-        rPP_r = tPM * pst**4 / phase**3
-        rMM_r = 0.0j * ngrid
-    rMP_r = - t / phase**2 * pst**3
-    rPM_r = - t / phase**2 * pst**3
-
-    if reversed:
         rPP_l = - tPM * phase
         rMM_l = 0.0j * ngrid
     else:
+        rPP_r = tPM * pst**4 / phase**3
+        rMM_r = 0.0j * ngrid
         rPP_l = 0.0j * ngrid
         rMM_l = - tPM * phase
+    rMP_r = - t / phase**2 * pst**3
+    rPM_r = - t / phase**2 * pst**3
     rMP_l = t * phase**2 / pst
     rPM_l = t * phase**2 / pst
 
-    t_right = [tPP_r, tMP_r, tPM_r, tMM_r]  # 2x2 scattering matrices
-    t_left = [tPP_l, tMP_l, tPM_l, tMM_l]
+    # 2x2 scattering matrices
+    t_right = [tPP_r, tMP_r, tPM_r, tMM_r]
+    t_left  = [tPP_l, tMP_l, tPM_l, tMM_l]
     r_right = [rPP_r, rMP_r, rPM_r, rMM_r]
-    r_left = [rPP_l, rMP_l, rPM_l, rMM_l]
+    r_left  = [rPP_l, rMP_l, rPM_l, rMM_l]
 
     return [t_right, t_left, r_right, r_left]
 
@@ -95,80 +90,26 @@ scatTOT = [chirality_preserving_mirror_scatmat(omegaPR,gammaPR,omega,reversed=Fa
 
 # #####################################################################
 
+theta0 = 0 # INCIDENT ANGLE
+
+ngrid = np.ones_like(omega)
+
 l = np.linspace(150, 450, 20)
 ampl = list()
-
 for dist in l:
+    # five layers: air, chirality-preserving mirror, air, reversed
+    # chirality-preserving mirror, air note that since the chirality-preserving
+    # mirrors are handled by directly passing the scattering matrices, the parameters d,n,mu,k do not matter for them.
 
-    ################
-    # INCIDENT ANGLE
-    ################
-    theta0 = 0
-    ################
+    mats = [  'air', 'Custom',   'air', 'Custom',   'air']
+    ds   = [ np.inf,       0.,    dist,       0.,  np.inf]
+    ns   = [  ngrid,    ngrid,   ngrid,    ngrid,   ngrid]
+    mus  = [  ngrid,    ngrid,   ngrid,    ngrid,   ngrid]
+    ks   = [0*ngrid,  0*ngrid, 0*ngrid,  0*ngrid, 0*ngrid]
 
-    ######
-    # AIR
-    ###############
-    n1 = 1 * ngrid
-    mu1 = 1 * ngrid
-    k1 = 0 * ngrid
-    d1 = np.inf
-    ###############
-
-    #####################
-    # PRESERVING MIRROR 1
-    #######################################
-    k2 = 0 * ngrid
-    mu2 = 1 * ngrid
-    n2 = 1 * ngrid
-    d2 = 0  # the distance has no influence
-    #######################################
-
-    #####
-    # AIR
-    ###############
-    n3 = 1 * ngrid
-    mu3 = 1 * ngrid
-    k3 = 0 * ngrid
-    d3 = dist
-    ###############
-
-    #####################
-    # PRESERVING MIRROR 2
-    ######################################
-    k4 = 0 * ngrid
-    mu4 = 1 * ngrid
-    n4 = 1 * ngrid
-    d4 = 0 # the distance has no influence
-    ######################################
-
-    #####
-    # AIR
-    ###############
-    n5 = 1 * ngrid
-    mu5 = 1 * ngrid
-    k5 = 0 * ngrid
-    d5 = np.inf
-    ###############
-
-    ########################################
-    # ALL THE ARRAYS OF THE INPUT PARAMETERS
-    ##################################################
-    nTOT = [n1, n2, n3, n4, n5]
-    muTOT = [mu1, mu2, mu3, mu4, mu5]
-    kTOT = [k1, k2, k3, k4, k5]
-    dTOT = [d1, d2, d3, d4, d5]
-    matTOT = ['air', 'Custom', 'air', 'Custom', 'air']
-    ##################################################
-
-    ###########################################
-    # CALLING OF THE CLASS FOR THE EMPTY CAVITY
-    #########################################################################
-    tScat = ts.TScat(theta0, nTOT, muTOT, kTOT, dTOT, omega, matTOT, scatTOT)
-    #########################################################################
+    tScat = ts.TScat(theta0, ns, mus, ks, ds, omega, mats, scatTOT)
 
     ampl.append(tScat.calc_ampl(2, [1,0], omega))  # field in cavity for an incoming LCP wave
-
 
 #############
 # OBSERVABLES
