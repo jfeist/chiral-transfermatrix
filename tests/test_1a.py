rewrite code to consistently respect numpy broadcasting rules for input, i.e., a scan over one or several parameters can be done by just passing the correct input shape parameters, and no explicit loops are usually necessary

tests/test_1a.py
@@ -1,78 +1,41 @@
-###########
-# LIBRARIES
-#####################################################
-import tscat as ts  # Essential "import" to run TSCAT
-
+import tscat as ts
 import numpy as np
-#####################################################
 
-
-###################################################################
-# GENERAL DEFINITION OF THE DIELECTRIC FUNCTION AND CHIRAL COUPLING
-###############################################################################################
-def eps_DL(epsinf, omegap, omega, omega0=0, gamma=0, k0=0):
+def eps_DL(omega, epsinf, omegap, omega0=0, gamma=0, k0=0):
     eps = epsinf + (omegap**2 / ((omega0**2 - omega**2) - 1j * gamma * omega))
-    # dispersive dielectric function
-    n = np.sqrt(eps)
-
     if k0 != 0: # chiral coupling
         k = k0 * (omegap**2 * omega / (omega0 * ((omega0**2 - omega**2) - 1j * gamma * omega)))
     else:
         k = np.zeros_like(omega)
-    return eps, n, k
-###############################################################################################
+    return eps, np.sqrt(eps), k
 
-
-######################################################################
-# RANGE OF OMEGA AND CREATION OF THE CORRESPONDING ARRAY FOR THE INPUT
-######################################################################
 omega = np.linspace(1.6, 2.4, 30)  # Omega in eV
-ngrid = np.ones_like(omega)
-######################################################################
-
-################
-# INCIDENT ANGLE
-################
 theta0 = 0.231
-################
 
-epsinf = 4.77574276
-omegapMirr = 9.48300763
-eps_m, n_m, k_m = eps_DL(epsinf, omegapMirr, omega, omega0 = 0, gamma = 0.17486845, k0 = 0)
-metal_mirror = ts.MaterialLayer(n=n_m,k=k_m,mu=ngrid,d=30)
+eps_m, n_m, k_m = eps_DL(omega, epsinf=4.77574276, omegap=9.48300763, omega0=0, gamma=0.17486845, k0=0)
+metal_mirror = ts.MaterialLayer(n=n_m,k=k_m,mu=1,d=30)
 
-air_infty = ts.MaterialLayer(n=ngrid,k=0*ngrid,mu=ngrid,d=np.inf)
+air_infty = ts.MaterialLayer(n=1,k=0,mu=1,d=np.inf)
 
-tScats = []
-omegapChirals = np.linspace(0.0, 1.0, 20)
-for omegapChiral in omegapChirals:
-    # CHIRAL MATERIAL
-    epsinf = 2.89
-    eps3M, n3, k3 = eps_DL(epsinf, omegapChiral, omega, omega0 = 2.0, gamma = 0.05, k0 = 1e-3)
-    molecules = ts.MaterialLayer(n=n3,k=k3,mu=ngrid,d=150.)
+# we want to scan over the resonance strength omegapChiral AND the frequency
+# omega (i.e., output should be 100x100), so make this a 100x1 2D array
+# numpy broadcasting will take care of the rest (i.e., omega will be the second axis)
+omegapChiral = np.linspace(0.0, 1.0, 20)[:,None]
+eps3M, n3, k3 = eps_DL(omega, epsinf=2.89, omegap=omegapChiral, omega0=2.0, gamma=0.05, k0=1e-3)
+molecules = ts.MaterialLayer(n=n3,k=k3,mu=1,d=150.)
 
-    layers = [air_infty, metal_mirror, molecules, metal_mirror, air_infty]
-    tScats.append(ts.TScat(theta0, layers, omega))
-
-#############
-# OBSERVABLES
-#######################
-arr1 = np.array([ts.Tsp for ts in tScats])
-arr2 = np.array([ts.Tsm for ts in tScats])
-arr3 = np.array([ts.Rsp for ts in tScats])
-arr4 = np.array([ts.Rsm for ts in tScats])
-arr5 = np.array([ts.dct_s for ts in tScats])
-#######################
+layers = [air_infty, metal_mirror, molecules, metal_mirror, air_infty]
+tScat = ts.TScat(theta0, layers, omega)
 
 # np.savez_compressed('tests/test_1a.npz', omega=omega, coupl=coupl, Tplist=arr1, Tmlist=arr2, Rplist=arr3, Rmlist=arr4, DCTlist=arr5)
 def test_1a():
     ref_data = np.load('tests/test_1a.npz')
     assert np.allclose(omega, ref_data['omega'])
-    assert np.allclose(omegapChirals, ref_data['coupl'])
-    assert np.allclose(arr1, ref_data['Tplist'])
-    assert np.allclose(arr2, ref_data['Tmlist'])
-    assert np.allclose(arr3, ref_data['Rplist'])
-    assert np.allclose(arr4, ref_data['Rmlist'])
-    assert np.allclose(arr5, ref_data['DCTlist'])
+    assert np.allclose(omegapChiral.squeeze(), ref_data['coupl'])
+    assert np.allclose(tScat.Tsp, ref_data['Tplist'])
+    assert np.allclose(tScat.Tsm, ref_data['Tmlist'])
+    assert np.allclose(tScat.Rsp, ref_data['Rplist'])
+    assert np.allclose(tScat.Rsm, ref_data['Rmlist'])
+    assert np.allclose(tScat.dct_s, ref_data['DCTlist'])
 
     print('Test 1a passed!')

tests/test_2a.py
@@ -1,92 +1,45 @@
-###########
-# LIBRARIES
-######################################################
-import tscat as ts  # Essential "import" to run TSCAT
-
+import tscat as ts
 import numpy as np
-######################################################
 
-
-###################################################################
-# GENERAL DEFINITION OF THE DIELECTRIC FUNCTION AND CHIRAL COUPLING
-################################################################################################
-def eps_DL(epsinf, omegap, omega, omega0=0, gamma=0, k0=0):
+def eps_DL(omega, epsinf, omegap, omega0=0, gamma=0, k0=0):
     eps = epsinf + (omegap**2 / ((omega0**2 - omega**2) - 1j * gamma * omega))
-    # dispersive dielectric function
-    n = np.sqrt(eps)
-
     if k0 != 0: # chiral coupling
         k = k0 * (omegap**2 * omega / (omega0 * ((omega0**2 - omega**2) - 1j * gamma * omega)))
     else:
         k = np.zeros_like(omega)
-    return eps, n, k
-#################################################################################################
+    return eps, np.sqrt(eps), k
 
-######################################################################
-# RANGE OF OMEGA AND CREATION OF THE CORRESPONDING ARRAY FOR THE INPUT
-######################################################################
 omega = np.linspace(1.6, 2.4, 30)
-ngrid = np.ones_like(omega)
-######################################################################
+theta0 = 0.231
 
-###############################################################
-# DEFINITION OF THE SCATTERING MATRICES FOR PRESERVING MIRROR 1
-######################################################################
 omegaPR = 2.0
 gammaPR = 0.05
 
 mirror_1 = ts.chirality_preserving_mirror(omegaPR,gammaPR,omega,reversed=False)
 mirror_2 = ts.chirality_preserving_mirror(omegaPR,gammaPR,omega,reversed=True)
-air_infty = ts.MaterialLayer(n=ngrid,k=0*ngrid,mu=ngrid,d=np.inf)
-air_thin  = ts.MaterialLayer(n=ngrid,k=0*ngrid,mu=ngrid,d=0.01)
+air_infty = ts.MaterialLayer(n=1,k=0,mu=1,d=np.inf)
+air_thin  = ts.MaterialLayer(n=1,k=0,mu=1,d=0.01)
 
-coupl = np.linspace(0.0, 1.0, 20)
+# we want to scan over the resonance strength omegapChiral AND the frequency
+# omega (i.e., output should be 100x100), so make this a 100x1 2D array
+# numpy broadcasting will take care of the rest (i.e., omega will be the second axis)
+omegapChiral = np.linspace(0.0, 1.0, 20)[:,None]
+eps_mol, n_mol, k_mol = eps_DL(omega, epsinf=2.89, omegap=omegapChiral, omega0=2.0, gamma=0.05, k0=0.0)
+molecules = ts.MaterialLayer(n=n_mol,k=k_mol,mu=1,d=180.)
 
-tScats = []
-for coup in coupl:
+layers = [air_infty, mirror_1, air_thin, molecules, air_thin, mirror_2, air_infty]
 
-    ################
-    # INCIDENT ANGLE
-    ################
-    theta0 = 0.231
-    ################
-
-    #################
-    # CHIRAL MATERIAL
-    #########################################################################################
-    epsinf = 2.89
-    omegapChiral = coup
-    eps4M, n4, k4 = eps_DL(epsinf, omegapChiral, omega, omega0 = 2.0, gamma = 0.05, k0 = 0.0)
-    molecules = ts.MaterialLayer(n=n4,k=k4,mu=ngrid,d=180.)
-    #########################################################################################
-
-    layers = [air_infty, mirror_1, air_thin, molecules, air_thin, mirror_2, air_infty]
-
-    ###########################################
-    # CALLING OF THE CLASS FOR THE EMPTY CAVITY
-    #########################################################################
-    tScats.append(ts.TScat(theta0, layers, omega))
-    #########################################################################
-
-#############
-# OBSERVABLES
-#######################
-arr1 = np.array([ts.Tsp for ts in tScats])
-arr2 = np.array([ts.Tsm for ts in tScats])
-arr3 = np.array([ts.Rsp for ts in tScats])
-arr4 = np.array([ts.Rsm for ts in tScats])
-arr5 = np.array([ts.dct_s for ts in tScats])
-#######################
+tScat = ts.TScat(theta0, layers, omega)
 
 # np.savez_compressed('tests/test_2a.npz', omega=omega, coupl=coupl, Tplist=arr1, Tmlist=arr2, Rplist=arr3, Rmlist=arr4, DCTlist=arr5)
 def test_2a():
     ref_data = np.load('tests/test_2a.npz')
     assert np.allclose(omega, ref_data['omega'])
-    assert np.allclose(coupl, ref_data['coupl'])
-    assert np.allclose(arr1, ref_data['Tplist'])
-    assert np.allclose(arr2, ref_data['Tmlist'])
-    assert np.allclose(arr3, ref_data['Rplist'])
-    assert np.allclose(arr4, ref_data['Rmlist'])
-    assert np.allclose(arr5, ref_data['DCTlist'])
+    assert np.allclose(omegapChiral.squeeze(), ref_data['coupl'])
+    assert np.allclose(tScat.Tsp, ref_data['Tplist'])
+    assert np.allclose(tScat.Tsm, ref_data['Tmlist'])
+    assert np.allclose(tScat.Rsp, ref_data['Rplist'])
+    assert np.allclose(tScat.Rsm, ref_data['Rmlist'])
+    assert np.allclose(tScat.dct_s, ref_data['DCTlist'])
 
     print('Test 2a passed!')

tests/test_3a.py
@@ -1,49 +1,30 @@
-###########
-# LIBRARIES
-######################################################
-import tscat as ts  # Essential "import" to run TSCAT
-
+import tscat as ts
 import numpy as np
-######################################################
 
-######################################################################
-# RANGE OF OMEGA AND CREATION OF THE CORRESPONDING ARRAY FOR THE INPUT
-######################################################################
+# we want to scan over frequency omega, and cavity length l, as two different
+# axis. TScat is "transparent" to numpy broadcasting, so we can just make omega
+# a 1D array and l a 2D array (shape nl x 1), and numpy will take care of the
+# rest. The results will be 2D arrays of shape nl x nomega
 omega = np.linspace(1.8, 2.2, 30)
-ngrid = np.ones_like(omega)
-######################################################################
-
-######################################################################
-# DEFINITION OF THE SCATTERING MATRICES FOR PRESERVING MIRRORS
-######################################################################
+l = np.linspace(150, 450, 20)[:,None]
 omegaPR = 2.0
 gammaPR = 0.05
-
 mirror_1 = ts.chirality_preserving_mirror(omegaPR,gammaPR,omega,reversed=False)
 mirror_2 = ts.chirality_preserving_mirror(omegaPR,gammaPR,omega,reversed=True)
-air_infty = ts.MaterialLayer(n=ngrid,k=0*ngrid,mu=ngrid,d=np.inf)
-
-# #####################################################################
+air_infty = ts.MaterialLayer(n=1,k=0,mu=1,d=np.inf)
+air_cavity = ts.MaterialLayer(n=1,k=0,mu=1,d=l)
+layers = [air_infty, mirror_1, air_cavity, mirror_2, air_infty]
 
 theta0 = 0.231 # INCIDENT ANGLE
 
-l = np.linspace(150, 450, 20)
-ampl = list()
-for dist in l:
-    air_cavity = ts.MaterialLayer(n=ngrid,k=0*ngrid,mu=ngrid,d=dist)
-    layers = [air_infty, mirror_1, air_cavity, mirror_2, air_infty]
-    tScat = ts.TScat(theta0, layers, omega)
+tScat = ts.TScat(theta0, layers, omega)
 
-    ampl.append(tScat.field_ampl(2, [1,0]))  # field in cavity for an incoming LCP wave
+ampl = tScat.field_ampl(2, [1,0])  # field in cavity for an incoming LCP wave
 
-#############
-# OBSERVABLES
-#####################################################################################
-ampl2 = np.array(ampl)
-Elp = ampl2[:, :, 0]
-Elm = ampl2[:, :, 1]
-Erp = ampl2[:, :, 2]
-Erm = ampl2[:, :, 3]
+Elp = ampl[:, :, 0]
+Elm = ampl[:, :, 1]
+Erp = ampl[:, :, 2]
+Erm = ampl[:, :, 3]
 lcp = abs(Elp)**2 + abs(Erp)**2  # total LCP in layer 2 (inside the cavity)
 rcp = abs(Elm)**2 + abs(Erm)**2  # total RCP in layer 2 (inside the cavity)
 #####################################################################################

tscat.py
@@ -13,21 +13,21 @@ class Layer:
 class MaterialLayer(Layer):
     """A layer made of a material described by its optical constants and thickness."""
     def __init__(self,n,k,mu,d,name=""):
-        self.n = np.asarray(n)
-        self.k = np.asarray(k)
-        self.mu = np.asarray(mu)
-        self.d = d
+        self.n = np.atleast_1d(n)
+        self.k = np.atleast_1d(k)
+        self.mu = np.atleast_1d(mu)
+        self.d = np.atleast_1d(d)
         self.name = name
 
         # REFRACTIVE INDICES OF CHIRAL MEDIUM
         npl = self.n * np.sqrt(self.mu) * (1 + self.k)  # refractive index n+
         npm = self.n * np.sqrt(self.mu) * (1 - self.k)  # refractive index n-
-        # nps has indices [omega, polarization]
-        self.nps = np.column_stack((npl,npm))
+        # nps has indices [input_indices..., polarization]
+        self.nps = np.stack((npl,npm),axis=-1)
 
     def set_costheta(self, nsinthetas):
         # use that cos(asin(x)) = sqrt(1-x**2)
-        # costhetas has indices [omega, polarization]
+        # costhetas has indices [input_indices..., polarization]
         self.costhetas = np.sqrt(1 - (nsinthetas / self.nps)**2)
 
     # propagation phases across layer (diagonal of a diagonal matrix)
@@ -35,8 +35,8 @@ class MaterialLayer(Layer):
         # constant is 1/ħc in units of 1/(eV nm), which converts
         # from omega in eV (i.e., it is really ħω) to k in nm^-1
         kd = 0.005067730716156395 * omega * self.d
-        phis = kd[:,None] * self.nps * self.costhetas
-        phil = np.hstack((-phis, phis))  # array of phases
+        phis = kd[...,None] * self.nps * self.costhetas
+        phil = np.concatenate((-phis, phis), axis=-1)  # array of phases
         return np.exp(1j*phil)
 
     # transfer matrix from previous layer (on the left) to this one
@@ -47,10 +47,8 @@ class MaterialLayer(Layer):
 class TransferMatrixLayer(Layer):
     """A layer with a fixed transfer matrix (assumed to be from and to air)."""
     def __init__(self,M,name=""):
-        assert M.ndim == 3
-        assert M.shape[1:] == (4,4)
+        assert M.shape[-2:] == (4,4)
         self.M = M
-        self.nomega = M.shape[0]
         self.n = 1.
         self.mu = 1.
         self.name = name
@@ -59,7 +57,7 @@ class TransferMatrixLayer(Layer):
         self.costhetas = np.sqrt(1 - nsinthetas**2)
 
     def phase_matrix_diagonal(self, omega):
-        return np.ones((self.nomega, 4))
+        return np.ones(self.M.shape[:-1])
 
     def transfer_matrix(self, prev):
         return self.M
@@ -83,17 +81,17 @@ class TScat:
         # total transfer matrix
         self.M = self.M12[0] # M of the first interface
         for a,b in zip(self.phas, self.M12[1:]): # cycle to add a phase and a successive interface
-            c = a[:,:,None] * b # A @ b where A_wij = delta_ij a_wj
+            c = a[...,None] * b # A @ b where A_[...]ij = delta_ij a_[...]j
             self.M = self.M @ c
 
         # convert from the transfer matrix (connecting amplitudes on the left and
         # right of an interface) to the scattering matrix (connecting incoming and
         # outgoing amplitudes). the matrices are 2x2 blocks, where the index
         # within each block is for left and right circular polarizations
-        tt  = self.M[:, 0:2, 0:2]  # transmission block upper left
-        trp = self.M[:, 0:2, 2:4]  # reflection block upper right
-        tr  = self.M[:, 2:4, 0:2]  # reflection block lower left
-        ttp = self.M[:, 2:4, 2:4]  # transmission block lower right
+        tt  = self.M[..., 0:2, 0:2]  # transmission block upper left
+        trp = self.M[..., 0:2, 2:4]  # reflection block upper right
+        tr  = self.M[..., 2:4, 0:2]  # reflection block lower left
+        ttp = self.M[..., 2:4, 2:4]  # transmission block lower right
         tti = np.linalg.inv(tt)  # inversion of transmission block upper left
         self.Rs = tr @ tti    # reflection matrix for incidence from the left
         self.Ts = tti         # transmission matrix for incidence from the left
@@ -103,10 +101,10 @@ class TScat:
         # Calculate transmittance, reflectance, and DCT/DCR
 
         # the sum is over polarization of the outgoing field
-        self.Rsp, self.Rsm = np.sum(abs(self.Rs)**2, axis=1).T # reflectance +/- for incidence from the left
-        self.Tsp, self.Tsm = np.sum(abs(self.Ts)**2, axis=1).T # transmittance +/- for incidence from the left
-        self.Rdp, self.Rdm = np.sum(abs(self.Rd)**2, axis=1).T # reflectance +/- for incidence from the right
-        self.Tdp, self.Tdm = np.sum(abs(self.Td)**2, axis=1).T # transmittance +/- for incidence from the right
+        self.Rsp, self.Rsm = np.moveaxis(np.sum(abs(self.Rs)**2, axis=-2), -1, 0) # reflectance +/- for incidence from the left
+        self.Tsp, self.Tsm = np.moveaxis(np.sum(abs(self.Ts)**2, axis=-2), -1, 0) # transmittance +/- for incidence from the left
+        self.Rdp, self.Rdm = np.moveaxis(np.sum(abs(self.Rd)**2, axis=-2), -1, 0) # reflectance +/- for incidence from the right
+        self.Tdp, self.Tdm = np.moveaxis(np.sum(abs(self.Td)**2, axis=-2), -1, 0) # transmittance +/- for incidence from the right
         self.dct_s = calc_dct(self.Tsp, self.Tsm)  # dct for incidence from the left
         self.dcr_s = calc_dct(self.Rsp, self.Rsm)  # dcr for incidence from the left
         self.dct_r = calc_dct(self.Tdp, self.Tdm)  # dct for incidence from the right
@@ -117,19 +115,23 @@ class TScat:
         right end), given the amplitudes of the incoming fields from the
         left."""
 
+        # matrix-vector multiplication for arrays of matrices and vectors
+        matvec_mul = lambda A,v: np.einsum('...ij,...j->...i', A, v)
+
         # get coefficients on the right for input from the left
         # this is just the transmitted field, there is no incoming field from the right
-        self.fwd2 = np.zeros(self.M.shape[:2], dtype=complex)
-        self.fwd2[:, 0:2] = np.einsum("wij,wj->wi", self.Ts, np.atleast_2d(cinc))
+        self.fwd2 = np.zeros(self.M.shape[:-1], dtype=complex)
+        self.fwd2[..., 0:2] = matvec_mul(self.Ts,np.atleast_1d(cinc))
 
         if layer==len(self.layers)-1:
             return self.fwd2
 
         # now successively apply the transfer matrices from right to left to get
         # the field amplitude on the right of each layer
-        self.fwd2 = np.einsum("wij,wj->wi", self.M12[-1], self.fwd2)
+        self.fwd2 = matvec_mul(self.M12[-1], self.fwd2)
         for a,b in zip(self.phas[layer:][::-1], self.M12[layer:-1][::-1]):
-            self.fwd2 = np.einsum("wij,wj->wi", b, a*self.fwd2)
+            self.fwd2 *= a
+            self.fwd2 = matvec_mul(b, self.fwd2)
 
         return self.fwd2
 #########################################################################################
@@ -137,10 +139,10 @@ class TScat:
 # transfer matrix for an interface from material 1 to 2 (left to right)
 def transfer_matrix(n1, mu1, costhetas_1, n2, mu2, costhetas_2):
     et = (n2 / n1) * np.sqrt(mu1 / mu2)  # ratio of impendances
-    ratiocos = costhetas_2[:,None,:] / costhetas_1[:,:,None] # ratio of cosines of the structure of matrix
+    ratiocos = costhetas_2[...,None,:] / costhetas_1[...,:,None] # ratio of cosines of the structure of matrix
     par_tr = np.array([[1,-1],[-1,1]]) # matrix to change the sign of the matrix elements to fill correctly
-    Mt = (et[:,None,None] + par_tr) * (1 + par_tr * ratiocos) / 4 # array of the transmission matrix
-    Mr = (et[:,None,None] + par_tr) * (1 - par_tr * ratiocos) / 4 # array of the reflection matrix
+    Mt = (et[...,None,None] + par_tr) * (1 + par_tr * ratiocos) / 4 # array of the transmission matrix
+    Mr = (et[...,None,None] + par_tr) * (1 - par_tr * ratiocos) / 4 # array of the reflection matrix
     return np.block([[Mt,Mr],[Mr,Mt]])
 
 # differential chiral transmission (or reflection, same formula)
@@ -148,55 +150,41 @@ def calc_dct(Tp, Tm):
     return 2 * (Tp - Tm) / (Tp + Tm)  # Tp is the transmission + and Tm the transmission -
 
 def chirality_preserving_mirror_scatmat(omegaPR,gammaPR,omega,reversed=False):
-    ngrid = np.ones_like(omega)
-    tP =  gammaPR / (1j * (omega - omegaPR) + gammaPR)
+    tP = gammaPR / (1j * (omega - omegaPR) + gammaPR)
     rM = abs(tP)
     phase = tP / rM
     tPM = abs(tP)
-    t = np.sqrt((1 - np.abs(tPM)**2) / 2.0)
-    phit = np.pi / 2
-    pst = np.exp(1j * phit)
+    t = np.sqrt((1 - np.abs(tPM)**2) / 2)
+    pst = np.exp(1j * np.pi / 2)
 
-    tPP_r = t * pst
-    tMM_r = t * pst
-    tPP_l = t * pst
-    tMM_l = t * pst
+    tPP_r = tMM_r = tPP_l = tMM_l = t * pst
+    rMP_r = rPM_r = - t / phase**2 * pst**3
+    rMP_l = rPM_l = t * phase**2 / pst
     if reversed:
-        tMP_r = tPM * phase
-        tPM_r = 0.0j * ngrid
-        tMP_l = 0.0j * ngrid
-        tPM_l = tPM * phase
-    else:
-        tMP_r = 0.0j * ngrid
-        tPM_r = tPM * phase
-        tMP_l = tPM * phase
-        tPM_l = 0.0j * ngrid
+        tMP_r = tPM_l = tPM * phase
+        tPM_r = tMP_l = 0 * tMP_r
 
-    if reversed:
-        rPP_r = 0.0j * ngrid
         rMM_r = tPM * pst**4 / phase**3
         rPP_l = - tPM * phase
-        rMM_l = 0.0j * ngrid
+        rPP_r = rMM_l = 0 * rMM_r
     else:
+        tPM_r = tMP_l = tPM * phase
+        tMP_r = tPM_l = 0 * tPM_r
+
         rPP_r = tPM * pst**4 / phase**3
-        rMM_r = 0.0j * ngrid
-        rPP_l = 0.0j * ngrid
         rMM_l = - tPM * phase
-    rMP_r = - t / phase**2 * pst**3
-    rPM_r = - t / phase**2 * pst**3
-    rMP_l = t * phase**2 / pst
-    rPM_l = t * phase**2 / pst
+        rMM_r = rPP_l = 0 * rPP_r
 
-    # nomega x 2 x 2 scattering matrices
-    t_right = np.column_stack((tPP_r, tMP_r, tPM_r, tMM_r)).reshape(-1,2,2)
-    t_left  = np.column_stack((tPP_l, tMP_l, tPM_l, tMM_l)).reshape(-1,2,2)
-    r_right = np.column_stack((rPP_r, rMP_r, rPM_r, rMM_r)).reshape(-1,2,2)
-    r_left  = np.column_stack((rPP_l, rMP_l, rPM_l, rMM_l)).reshape(-1,2,2)
-
+    # [tPP_r.shape] x 2 x 2 scattering matrices
+    mshape = tPP_r.shape + (2,2)
+    t_right = np.column_stack((tPP_r, tMP_r, tPM_r, tMM_r)).reshape(mshape)
+    t_left  = np.column_stack((tPP_l, tMP_l, tPM_l, tMM_l)).reshape(mshape)
+    r_right = np.column_stack((rPP_r, rMP_r, rPM_r, rMM_r)).reshape(mshape)
+    r_left  = np.column_stack((rPP_l, rMP_l, rPM_l, rMM_l)).reshape(mshape)
     return t_right, t_left, r_right, r_left
 
 # convert scattering matrix S to transfer matrix M
-def S_to_M(scat): # no mur in this function (no magnetic field)
+def S_to_M(scat):
     Jt, Jte, Jre, Jr = scat
 
     Mt = np.linalg.inv(Jt)  # Inversion of the Jt matrix to construct the submatrix 2x2 for the transmission