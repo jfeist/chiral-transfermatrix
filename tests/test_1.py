9ea27e36521df610c7f555c4838987af597d5694
Johannes Feist <johannes.feist@gmail.com>
2023-07-01 15:25:54 +0200

    moved tests to tests/ directory and set up to work with pytest (i.e., just run py.test to test everything)

--- tests/test_1.py
+++ tests/test_1.py
@@ -7,26 +7,26 @@ def eps_DL(omega, epsinf, omegap, omega0=0, gamma=0, k0=0):
     k = 0*eps if k0==0 else k0 * omega / omega0 * (eps-epsinf)
     return eps, k
 
-omega = np.linspace(1.6, 2.4, 30)  # Omega in eV
-theta0 = 0
-
-eps_met, k_met = eps_DL(omega, epsinf=4.77574276, omegap=9.48300763, omega0=0, gamma=0.17486845, k0=0)
-metal_mirror = ts.MaterialLayer(d=30,eps=eps_met,kappa=k_met)
-
-air_infty = ts.MaterialLayer(d=np.inf,eps=1)
-
-# we want to scan over the resonance strength omegapChiral AND the frequency
-# omega (i.e., output should be 100x100), so make this a 100x1 2D array
-# numpy broadcasting will take care of the rest (i.e., omega will be the second axis)
-omegapChiral = np.linspace(0.0, 1.0, 20)[:,None]
-eps_mol, k_mol = eps_DL(omega, epsinf=2.89, omegap=omegapChiral, omega0=2.0, gamma=0.05, k0=1e-3)
-molecules = ts.MaterialLayer(d=150.,eps=eps_mol,kappa=k_mol)
-
-layers = [air_infty, metal_mirror, molecules, metal_mirror, air_infty]
-tScat = ts.TScat(layers, omega, theta0)
-
-# np.savez_compressed('tests/test_1.npz', omega=omega, coupl=coupl, Tplist=arr1, Tmlist=arr2, Rplist=arr3, Rmlist=arr4, DCTlist=arr5)
 def test_1():
+    omega = np.linspace(1.6, 2.4, 30)  # Omega in eV
+    theta0 = 0
+
+    eps_met, k_met = eps_DL(omega, epsinf=4.77574276, omegap=9.48300763, omega0=0, gamma=0.17486845, k0=0)
+    metal_mirror = ts.MaterialLayer(d=30,eps=eps_met,kappa=k_met)
+
+    air_infty = ts.MaterialLayer(d=np.inf,eps=1)
+
+    # we want to scan over the resonance strength omegapChiral AND the frequency
+    # omega (i.e., output should be 100x100), so make this a 100x1 2D array
+    # numpy broadcasting will take care of the rest (i.e., omega will be the second axis)
+    omegapChiral = np.linspace(0.0, 1.0, 20)[:,None]
+    eps_mol, k_mol = eps_DL(omega, epsinf=2.89, omegap=omegapChiral, omega0=2.0, gamma=0.05, k0=1e-3)
+    molecules = ts.MaterialLayer(d=150.,eps=eps_mol,kappa=k_mol)
+
+    layers = [air_infty, metal_mirror, molecules, metal_mirror, air_infty]
+    tScat = ts.TScat(layers, omega, theta0)
+
+    # np.savez_compressed('tests/test_1.npz', omega=omega, coupl=coupl, Tplist=arr1, Tmlist=arr2, Rplist=arr3, Rmlist=arr4, DCTlist=arr5)
     ref_data = np.load('tests/test_1.npz')
     assert np.allclose(omega, ref_data['omega'])
     assert np.allclose(omegapChiral.squeeze(), ref_data['coupl'])
@@ -35,5 +35,3 @@ def test_1():
     assert np.allclose(tScat.Rsp, ref_data['Rplist'])
     assert np.allclose(tScat.Rsm, ref_data['Rmlist'])
     assert np.allclose(tScat.DCTs, ref_data['DCTlist'])
-
-    print('Test 1 passed!')
--- tests/test_3a.py
+++ tests/test_3a.py
@@ -1,36 +1,36 @@
 import tscat as ts
 import numpy as np
 
-# we want to scan over frequency omega, and cavity length l, as two different
-# axis. TScat is "transparent" to numpy broadcasting, so we can just make omega
-# a 1D array and l a 2D array (shape nl x 1), and numpy will take care of the
-# rest. The results will be 2D arrays of shape nl x nomega
-omega = np.linspace(1.8, 2.2, 30)
-l = np.linspace(150, 450, 20)[:,None]
-omegaPR = 2.0
-gammaPR = 0.05
-mirror_1 = ts.helicity_preserving_mirror(omega,omegaPR,gammaPR,reversed=False)
-mirror_2 = ts.helicity_preserving_mirror(omega,omegaPR,gammaPR,reversed=True)
-air_infty  = ts.MaterialLayer(d=np.inf,eps=1)
-air_cavity = ts.MaterialLayer(d=l,     eps=1)
-layers = [air_infty, mirror_1, air_cavity, mirror_2, air_infty]
-
-theta0 = 0.231 # INCIDENT ANGLE
-
-tScat = ts.TScat(layers, omega, theta0)
-
-ampl = tScat.field_ampl(2, [1,0])  # field in cavity for an incoming LCP wave
-
-Elp = ampl[:, :, 0]
-Elm = ampl[:, :, 1]
-Erp = ampl[:, :, 2]
-Erm = ampl[:, :, 3]
-lcp = abs(Elp)**2 + abs(Erp)**2  # total LCP in layer 2 (inside the cavity)
-rcp = abs(Elm)**2 + abs(Erm)**2  # total RCP in layer 2 (inside the cavity)
-#####################################################################################
-
-# np.savez_compressed('tests/test_3a.npz', omega=omega, ELp=Elp, ELm=Elm, ERp=Erp, ERm=Erm, lcp=lcp, rcp=rcp)
 def test_3a():
+    # we want to scan over frequency omega, and cavity length l, as two different
+    # axis. TScat is "transparent" to numpy broadcasting, so we can just make omega
+    # a 1D array and l a 2D array (shape nl x 1), and numpy will take care of the
+    # rest. The results will be 2D arrays of shape nl x nomega
+    omega = np.linspace(1.8, 2.2, 30)
+    l = np.linspace(150, 450, 20)[:,None]
+    omegaPR = 2.0
+    gammaPR = 0.05
+    mirror_1 = ts.helicity_preserving_mirror(omega,omegaPR,gammaPR,reversed=False)
+    mirror_2 = ts.helicity_preserving_mirror(omega,omegaPR,gammaPR,reversed=True)
+    air_infty  = ts.MaterialLayer(d=np.inf,eps=1)
+    air_cavity = ts.MaterialLayer(d=l,     eps=1)
+    layers = [air_infty, mirror_1, air_cavity, mirror_2, air_infty]
+
+    theta0 = 0.231 # INCIDENT ANGLE
+
+    tScat = ts.TScat(layers, omega, theta0)
+
+    ampl = tScat.field_ampl(2, [1,0])  # field in cavity for an incoming LCP wave
+
+    Elp = ampl[:, :, 0]
+    Elm = ampl[:, :, 1]
+    Erp = ampl[:, :, 2]
+    Erm = ampl[:, :, 3]
+    lcp = abs(Elp)**2 + abs(Erp)**2  # total LCP in layer 2 (inside the cavity)
+    rcp = abs(Elm)**2 + abs(Erm)**2  # total RCP in layer 2 (inside the cavity)
+    #####################################################################################
+
+    # np.savez_compressed('tests/test_3a.npz', omega=omega, ELp=Elp, ELm=Elm, ERp=Erp, ERm=Erm, lcp=lcp, rcp=rcp)
     ref_data = np.load('tests/test_3a.npz')
     assert np.allclose(omega, ref_data['omega'])
     assert np.allclose(Elp, ref_data['ELp'])
@@ -39,5 +39,3 @@ def test_3a():
     assert np.allclose(Erm, ref_data['ERm'])
     assert np.allclose(lcp, ref_data['lcp'])
     assert np.allclose(rcp, ref_data['rcp'])
-
-    print('Test 3a passed!')
